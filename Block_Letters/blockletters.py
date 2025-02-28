297256710ca4efd7208fa7cf07fc11fff59a0ff4
hzd4m <120053935+hzd4m@users.noreply.github.com>
2025-02-28 22:14:37 +0000

    Primeiro projeto do repositório!

---
+++ Block_Letters/blockletters.py
@@ -0,0 +1,19 @@
+# Meu nome é : Zd4 
+# Eu odeio café frio  
+
+# Exemplo de arte ASCII para a letra 'Z'
+print("ZZZZZ")
+print("   Z")
+print("  Z")
+print(" Z")
+print("ZZZZZ")
+
+print()  # Linha em branco para separar as letras
+
+# Exemplo de arte ASCII para a letra 'D'
+print("DDDDD")
+print("D    D")
+print("D     D")
+print("D    D")
+print("DDDDD") 
+
